a17c2681653325a04ed31827791402a4c3267e93
Jyotish547 <jyotish547@gmail.com>
2023-10-22 18:37:35 -0600

    Files Initiated

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './bundle.ts'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/home.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+};
